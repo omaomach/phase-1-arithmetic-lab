5e8345f3df481ec4aa964f7e382c55ae21dff6b3
Machoka Joash Omao <aaronjoash44@gmail.com>
2022-08-16 11:52:29 +0300

    completed js arithmethic lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+const random = Math.floor(Math.random() * 10) + 1;
+console.log(random);
+
+let num3 = 16;
+let num4 = 6;
+const mod = num3 % num4;
+console.log(mod);
+
+let max = Math.max(1,2,5,6,8,9,12,16,20);
+console.log(max);
+
+
+
